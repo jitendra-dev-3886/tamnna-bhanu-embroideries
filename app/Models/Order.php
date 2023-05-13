2551290055c19ac237c4015b019a2782de3c2a6c
Shrikha Khalasi <shrikha.easternts@gmail.com>
2023-05-13 14:42:30 +0530

    stock decrease issue resolved

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -217,6 +217,8 @@ class Order extends Model
             $productData['quantity']  = $cartProduct->quantity;
 
             OrderProduct::create($productData); //Insert order products into order_products table
+            $product['stock'] = $product->stock - $cartProduct->quantity;
+            $product->update();
             Cart::where('user_id', $order->user_id)->where('product_id', $cartProduct->product_id)->delete(); //Delete products from cart
         }
 
